c79b66ddcb23498ee5aba97f9d02d531fc3606d9
Nico Casiean <casiean2@gmail.com>
2023-10-05 13:52:28 +0200

    Subiendo ejercicio 7

---
+++ React/Ejercicios/ejercicios/src/components/ejercicio7/HelloWorldH1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HelloWolrd() {
+  const hello = <h2>Hello, World!</h2>;
+
+  return (
+    <div>
+      {hello}
+    </div>
+  );
+}
+
+export default HelloWolrd;
