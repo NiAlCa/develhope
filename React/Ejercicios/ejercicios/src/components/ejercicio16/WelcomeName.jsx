39cb17ae70630927556dad1366e02a18bb1095bc
Nico Casiean <casiean2@gmail.com>
2023-10-06 03:12:45 +0200

    Subiendo ejercicios

--- React/Ejercicios/ejercicios/src/components/ejercicio16/WelcomeName.jsx
+++ React/Ejercicios/ejercicios/src/components/ejercicio16/WelcomeName.jsx
@@ -1,15 +1,20 @@
 //ejercicio 16, 17 , 18 y 19
 
+import { Age } from "../ejercicio20/age";
+
 // eslint-disable-next-line react/prop-types
-export function WelcomeName({name, age}){
+export function WelcomeName({ name, age }) {
+  name = "no name";
 
-    name = "no name";
-    
-    return(
-        <div>
-            <p><strong>Welcome, {name}! Your age is {age}</strong></p>
+  return (
+    <div>
+      <p>
+        <strong>
+          Welcome, {name}! {<Age age={age} />}
+        </strong>
+      </p>
 
-            {/* 19  Respueste esta pregunta en app jsx */}
-        </div>
-    )
+      {/* 19  Respueste esta pregunta en app jsx */}
+    </div>
+  );
 }
---
+++ React/Ejercicios/ejercicios/src/components/ejercicio20/Age.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line react/prop-types
+export function Age ({age}){
+
+ 
+
+    return(
+        <div>
+            <p>Your age is {age}</p>
+        </div>
+    )
+}
